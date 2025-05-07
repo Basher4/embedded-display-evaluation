34a7042f2c90a30055132698b7ede63c5548fc75
Matej Genci <matej.genci@gmail.com>
2025-05-07 21:00:34 +0100

    Set custom SPI pins
    I was fighting with setting custom SPI pins. All this time I was setting
    it to TX of SPI1, while using SPI0. D'oh.

--- GC9A01 Round Board/src/main.cpp
+++ GC9A01 Round Board/src/main.cpp
@@ -1,10 +1,9 @@
 #include <Arduino.h>
 #include <SPI.h>
-// #include <SoftwareSPI.h>
 #include <Arduino_GFX_Library.h>
 
-// constexpr int DISPLAY_SPI_SCK = 14;
-// constexpr int DISPLAY_SPI_SDA = 15;
+// constexpr int DISPLAY_SPI_SCK = 18;
+constexpr int DISPLAY_SPI_SDA = 7;
 constexpr int DISPLAY_SPI_CS = 20;
 constexpr int DISPLAY_DC = 17;
 constexpr int DISPLAY_RESET = 16;
@@ -14,23 +13,24 @@ Arduino_HWSPI bus(DISPLAY_DC, DISPLAY_SPI_CS);
 Arduino_GC9A01 display(&bus, DISPLAY_RESET, 2, true);
 
 void setup() {
-  Serial.begin(9600);
+    Serial.begin(9600);
 
-  // set SPI pins
-  // SPI.setSCK(DISPLAY_SPI_SCK);
-  // SPI.setTX(DISPLAY_SPI_SDA);
-  // SPI.setCS(DISPLAY_SPI_CS);
+    // set SPI pin
+    SPI.setMOSI(DISPLAY_SPI_SDA);
 
-  Serial.println("Drawing to the screen");
+    Serial.println("Drawing to the screen");
 
-  display.begin();
-  display.fillScreen(0x512B);
-  display.drawRect(50, 50, 50, 50, 0xFFFF);
-  display.setTextColor(RGB565_RED);
-  display.setCursor(100, 100);
-  display.println("Hello world");
+    display.begin();
+    display.fillScreen(0x512B);
+    display.setTextColor(RGB565_RED);
+
+    display.startWrite();
+    display.writeFillRect(50, 50, 50, 50, 0b01110'110100'00111);
+    display.setCursor(75, 75);
+    display.println("Hello world");
+    display.endWrite();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // put your main code here, to run repeatedly:
 }
